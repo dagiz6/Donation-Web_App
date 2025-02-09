433eee66df0a4f5ced5f858e3f3cab326929b8ef
dagiz6 <158800994+dagiz6@users.noreply.github.com>
2025-02-09 15:36:10 +0300

    deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/Donation-Web_App'>
+  <StrictMode>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </StrictMode>,
 )
