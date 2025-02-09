b95420caf2effda7051099e0dc69f823e3bd0657
dagiz6 <158800994+dagiz6@users.noreply.github.com>
2025-02-09 15:15:46 +0300

    Update router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Pages/NavBar/Navbar';
 import WelcomeScreen from './Pages/WelcomeScreen/WelcomeScreen';
 import DonationForm from './Pages/DonationForm/DonationForm';
