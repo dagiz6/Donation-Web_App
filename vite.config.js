2034046e90e272daba9850cd8b37d30379c4e3d6
dagiz6 <158800994+dagiz6@users.noreply.github.com>
2025-02-10 10:01:32 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()], 
-  base: '/Donation-Web_App/'
+  base: '/donation-web-app/'
 })
